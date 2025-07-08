d3000aaab69af05f7297ded6cce9dacea5c6bc01
voidhadex <emanuel1234567898234@gmail.com>
2025-07-08 15:00:48 -0300

    Add files via upload

---
+++ prog_princ.py
@@ -0,0 +1,13 @@
+import op_mat as op
+
+while True:
+    op=input('''Quais operações você deseja realizar?
+             1 - Soma
+             2 - Subtração
+             3 - Multiplicação
+             4 - Divisão
+             5 - Potência
+             6 - Raiz Quadrada
+             7 - Porcentagem
+             8 - Sair
+             Digite o número da operação: ''')
